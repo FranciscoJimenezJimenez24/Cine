12afd9518c954c57fbe6a726d9a88941cd483b93
FranciscoJimenezJimenez24 <franjimenezjimenez24@gmail.com>
2023-12-04 21:24:47 +0100

    Update

--- app/src/main/java/com/example/cine/FilmEditActivity.kt
+++ app/src/main/java/com/example/cine/FilmEditActivity.kt
@@ -4,12 +4,25 @@ import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Spinner
 import android.widget.Toast
 
 class FilmEditActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_film_edit)
+        val ivPoster:ImageView =findViewById(R.id.ivPoster)
+        val btnTakePicture:Button=findViewById(R.id.btnTakePicture)
+        val btnSelectImage:Button=findViewById(R.id.btnSelectImage)
+        val etTitleFilm: EditText =findViewById(R.id.etTitleFilm)
+        val etDirectorFilm: EditText =findViewById(R.id.etDirectorFilm)
+        val etYearFilm: EditText =findViewById(R.id.etYearFilm)
+        val etLinkIMDB: EditText =findViewById(R.id.etLinkIMDB)
+        val spinnerGender: Spinner =findViewById(R.id.spinnerGender)
+        val spinnerFormat: Spinner =findViewById(R.id.spinnerFormat)
+        val etNotes: EditText =findViewById(R.id.etNotes)
         val btnSave: Button = findViewById(R.id.btnSave)
         val btnCancel: Button = findViewById(R.id.btnCancel)
         btnSave.setOnClickListener { save() }
